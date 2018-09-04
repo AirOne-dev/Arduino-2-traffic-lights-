660e6e2b0644674748a305ae8b07a7e19854d641
Firologame <31392527+Firologame@users.noreply.github.com>
2018-09-04 15:41:23 +0200

    Update 2_feux_tricolor.ino

--- 2_feux_tricolor.ino
+++ 2_feux_tricolor.ino
@@ -1,8 +1,7 @@
 const int leds[6] = {1,2,3,4,5,6};
 
 void setup() {
-  for (int i = 0; i < 7; i++)
-  {
+  for (int i = 0; i < 7; i++) {
     pinMode(leds[i], OUTPUT);
     digitalWrite(leds[i], LOW);
   }
